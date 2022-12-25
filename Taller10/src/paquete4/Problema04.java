d5af8c6aea23aacde8d209e71d50a455e0299d81
Maria Paula Montaño <mpmontano@utpl.edu.ec>
2022-12-25 10:14:54 -0500

    Paula

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -5,10 +5,36 @@
  */
 package paquete4;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        double[][] datos = new double[2][2];
+
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.print("Ingrese un valor\n");
+                datos[i][j] = entrada.nextDouble();
+
+            }
+
+        }
+
+        
+              
+    }
     
 }
--- Taller10/src/paquete6/Problema06.java
+++ Taller10/src/paquete6/Problema06.java
@@ -11,4 +11,30 @@ package paquete6;
  */
 public class Problema06 {
     
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+                                {"Carrie Burton", "Lauren Rice"},
+                                {"Emma Escobar", "Lori Flores"},
+                                {"Steven West", "Toni Martin"}};
+        
+        
+        String reporte = "";
+        
+        for (int i = 0; i < estudiantes.length; i++) { 
+            for (int j = 0; j < estudiantes[i].length; j++) { 
+                if (estudiantes[i][j].length() == 11){
+                    System.out.printf("%s%s\n",
+                            reporte,
+                            estudiantes[i][j]);
+                }    
+            }
+            
+        }
+    }
 }
+       
