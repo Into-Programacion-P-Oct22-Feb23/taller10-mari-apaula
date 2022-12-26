89f2e99d59251c5cff892445f36dcde5cc5174fa
Maria Paula Montaño <mpmontano@utpl.edu.ec>
2022-12-26 11:30:10 -0500

    Maria Angel

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,33 +10,46 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
+
     public static void main(String[] args) {
         String[][] notasCualitativas = new String[3][3];
-	double[][] notasCuantitativas = {{7.1, 8, 10}, {3, 7, 9}, {10, 9, 2.1}};
-        double[] sumaNotas = new double[3];
-                            
-        
-        String reporte = "";
-        
-        for (int i = 0; i < notas.length; i++) { 
-            double suma = 0;
-            for (int j = 0; j < notas[i].length; j++) { 
-                suma = suma + notas[i][j];
-                     
+        double[][] notasCuantitativas = {{7.1, 8, 10}, {3, 7, 9}, {10, 9, 2.1}};
+        for (int i = 0; i < notasCuantitativas.length; i++) {
+            for (int j = 0; j < notasCuantitativas[i].length; j++) {
+                if (notasCuantitativas[i][j] >= 0 && notasCuantitativas[i][j]
+                        <= 2.9) {
+                    notasCualitativas[i][j] = "Insuficiente";
+                } else {
+                    if (notasCuantitativas[i][j] >= 3
+                            && notasCuantitativas[i][j] <= 4.9) {
+                        notasCualitativas[i][j] = "Regular";
+                    } else {
+                        if (notasCuantitativas[i][j] >= 5
+                                && notasCuantitativas[i][j] <= 7.9) {
+                            notasCualitativas[i][j] = "Buena";
+                        } else {
+                            if (notasCuantitativas[i][j] >= 8
+                                    && notasCuantitativas[i][j] <= 9.5) {
+                                notasCualitativas[i][j] = "Muy Buena";
+                            } else {
+                                if (notasCuantitativas[i][j] >= 9.6
+                                        && notasCuantitativas[i][j] <= 10) {
+                                    notasCualitativas[i][j] = "Sobresaliente";
+                                }
+                            }
+                        }
+                    }
+                }
             }
-            sumaNotas[i] = suma; // 
         }
-        
-        // Presentación de resultados
-        for (int i = 0; i < nombresEstudiante.length; i++) {
-            reporte = String.format("%sEstudiante %s tiene una sumataria "
-                    + "de notas: %.3f\n",
-                    reporte,
-                    nombresEstudiante[i],
-                    sumaNotas[i]);
+
+        for (int i = 0; i < notasCuantitativas.length; i++) {
+            for (int j = 0; j < notasCuantitativas[j].length; j++) {
+
+                System.out.printf("Nota cuantitativa: %.1f %s\n",
+                        notasCuantitativas[i][j], notasCualitativas[i][j]);
+
+            }
         }
-        
-        System.out.printf("%s\n", reporte);
-        
     }
 }
--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,35 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}};
+        char[][] inicial1 = new char[4][2];
+        String[][] inicial2 = new String[4][2];
+        String respuesta = "Listado de estudiantes que empiezan por S P T \n";
+        //guardar en respuesta los que empiezan por S P o T
+
+        for (int i = 0; i < estudiantes.length; i++) {
+            for (int j = 0; j < estudiantes[i].length; j++) {
+                inicial1[i][j] = estudiantes[i][j].charAt(0);
+                inicial2[i][j] = String.valueOf(inicial1[i][j]);
+
+            }
+        }
+        for (int i = 0; i < estudiantes.length; i++) {
+            for (int j = 0; j < estudiantes[i].length; j++) {
+                if (inicial2[i][j].equals("S")
+                        || inicial2[i][j].equals("P")
+                        || inicial2[i][j].equals("T")) {
+                    respuesta = String.format("%s%s\n",
+                            respuesta,
+                            estudiantes[i][j]);
+                }
+            }
+        }
+        System.out.println(respuesta);
+    }
 }
